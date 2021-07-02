b9b951ff6fb589a65808e7dbbcb5796f2fde6259
vladimirovmm <vladimirov.m.m@ya.ru>
2021-07-02 18:05:02 +0500

    for mac

--- dove_download.sh
+++ dove_download.sh
@@ -8,9 +8,7 @@ if [ ! -e $basefolder ]; then
 fi
 releases_path="$basefolder/releases.json"
 
-if [ ! -e $releases_path ] \
-    || ([[ "$OSTYPE" == "darwin"* ]] && [[ $($(date -j "+%s") - $(date "+%s" -r $releases_path)) -ge 600 ]]) \
-    || ([[ "$OSTYPE" != "darwin"* ]] && [[ $($(date "+%s") - $(date "+%s" -r $releases_path)) -ge 600 ]]); then
+if [ ! -e $releases_path ] || [[ $($(date -j "+%s") - $(date "+%s" -r $releases_path)) -ge 600 ]]; then
   echo "Download: releases.json"
   curl -o "$releases_path" \
       -s https://api.github.com/repos/pontem-network/move-tools/releases
